55b5f2cb4edc885f55b22151221eac7e90ff8af4
NormaN <normanargiolas@gmail.com>
2020-07-06 03:23:22 +0200

    second and third

--- stats.c
+++ stats.c
@@ -1,4 +1,3 @@
-
 /******************************************************************************
  * Copyright (C) 2017 by Alex Fosdick - University of Colorado
  *
@@ -28,23 +27,155 @@
 
 
 #include <stdio.h>
+#include <stdlib.h>
+
 #include "stats.h"
 
 /* Size of the Data Set */
 #define SIZE (40)
 
+// swap values with respect a type it
+#ifndef SWAP
+    #define SWAP(type, a, b) \
+    { \
+        type temp = a; \
+        a = b; \
+        b = temp; \
+    }
+#endif
+
+static unsigned char compare( const void* a, const void* b);
+static int reverseUnsignedCharArray(unsigned char *array, const size_t length);
+
+
+
 void main()
 {
 
   unsigned char test[SIZE] = { 34, 201, 190, 154,   8, 194,   2,   6,
-                              114, 88,   45,  76, 123,  87,  25,  23,
-                              200, 122, 150, 90,   92,  87, 177, 244,
-                              201,   6,  12,  60,   8,   2,   5,  67,
-                                7,  87, 250, 230,  99,   3, 100,  90};
+							  114, 88,   45,  76, 123,  87,  25,  23,
+							  200, 122, 150, 90,   92,  87, 177, 244,
+							  201,   6,  12,  60,   8,   2,   5,  67,
+								7,  87, 250, 230,  99,   3, 100,  90};
 
   /* Other Variable Declarations Go Here */
   /* Statistics and Printing Functions Go Here */
 
+  print_array(test, SIZE);
+  print_statistics(test, SIZE);
+
+  sort_array(test, SIZE);
+  print_array(test, SIZE);
+  print_statistics(test, SIZE);
+
 }
 
 /* Add other Implementation File Code Here */
+
+void print_array(unsigned char *test, unsigned int size)
+{
+	printf("--------------------\r\n");
+	for(unsigned int i = 0; i < size; i++)
+	{
+		printf("%d  ", test[i]);
+	}
+	printf("\r\n");
+
+	printf("--------------------\r\n");
+}
+
+unsigned char find_maximum(unsigned char *test, unsigned int size)
+{
+	unsigned char max = test[0];
+	for(unsigned int i = 0; i < size; i++)
+	{
+
+		if (test[i] > max)
+		{
+			max = test[i];
+		}
+	}
+	//printf("max: %d\r\n", max);
+
+	return max;
+}
+
+unsigned char find_minimum(unsigned char *test, unsigned int size)
+{
+	unsigned char max = test[0];
+	for(unsigned int i = 0; i < size; i++)
+	{
+
+		if (test[i] < max)
+		{
+			max = test[i];
+		}
+	}
+	//printf("max: %d\r\n", max);
+
+	return max;
+}
+
+float find_mean(unsigned char *test, unsigned int size)
+{
+	unsigned char s;
+	for(unsigned int i = 0; i < size; i++)
+	{
+		s += test[i];
+	}
+	float m = (float) s/size;
+
+//	printf("m: %f\r\n", m);
+
+	return m;
+}
+
+unsigned char find_median(unsigned char *test, unsigned int size)
+{
+
+	unsigned char t[SIZE];
+	memcpy (t, test, SIZE );
+	qsort( t, SIZE, sizeof(unsigned char), compare);
+	unsigned char m = t[SIZE/2];
+
+//	printf("m: %d\r\n", m);
+
+	return m;
+}
+
+
+int sort_array(unsigned char *test, unsigned int size)
+{
+
+	qsort( test, SIZE, sizeof(unsigned char), compare);
+	reverseUnsignedCharArray(test, SIZE);
+	//todo check error
+	return 0;
+}
+
+static int reverseUnsignedCharArray(unsigned char *array, const size_t length)
+{
+    for (int i = 0; i < length / 2; ++i) {
+        SWAP(unsigned char, array[i], array[length - i - 1]);
+    }
+    return 0;
+}
+
+static unsigned char compare (const void * a, const void * b)
+{
+  return ( *(unsigned char*)a - *(unsigned char*)b );
+}
+
+
+void print_statistics(const unsigned char *test, unsigned int size)
+{
+	printf("--------------------\r\n");
+
+	printf("minimum: %d\r\n", find_minimum(test, SIZE));
+	printf("maximum: %d\r\n", find_maximum(test, SIZE));
+	printf("mean: %f\r\n", find_mean(test, SIZE));
+	printf("median: %d\r\n", find_median(test, SIZE));
+
+	printf("--------------------\r\n");
+
+}
--- stats.h
+++ stats.h
@@ -24,18 +24,97 @@
 /* Add Your Declarations and Function Comments here */ 
 
 
+void print_array(unsigned char *test, unsigned int size);
 /**
- * @brief <Add Brief Description of Function Here>
+ * @brief prints the array to the screen
  *
- * <Add Extended Description Here>
+ * Given an array of data and a length, prints the array to the screen
  *
- * @param <Add InputName> <add description here>
- * @param <Add InputName> <add description here>
- * @param <Add InputName> <add description here>
- * @param <Add InputName> <add description here>
+ * @param test array of data
+ * @param size array length
  *
- * @return <Add Return Informaiton here>
+ * @return void
  */
 
+
+unsigned char find_maximum(unsigned char *test, unsigned int size);
+/**
+ * @brief returns the maximum of an array
+ *
+ * Given an array of data and a length, returns the maximum
+ *
+ * @param test array of data
+ * @param size array length
+ *
+ * @return unsigned char - the maximum of an array
+ */
+
+
+unsigned char find_minimum(unsigned char *test, unsigned int size);
+/**
+ * @brief returns the minimum of an array
+ *
+ * Given an array of data and a length, returns the minimum
+ *
+ * @param test array of data
+ * @param size array length
+ *
+ * @return unsigned char - the minimum of an array
+ */
+
+
+unsigned char find_median(unsigned char *test, unsigned int size);
+/**
+ * @brief returns the median value of an array
+ *
+ * Given an array of data and a length, returns the median value
+ *
+ * @param test array of data
+ * @param size array length
+ *
+ * @return float - the median of an array
+ */
+
+
+float find_mean(unsigned char *test, unsigned int size);
+/**
+ * @brief returns the mean value of an array
+ *
+ * Given an array of data and a length, returns the mean
+ *
+ * @param test array of data
+ * @param size array length
+ *
+ * @return float - the mean of an array
+ */
+
+
+int sort_array(unsigned char *test, unsigned int size);
+/**
+ * @brief sorts the array from largest to smallest
+ *
+ * Given an array of data and a length, sorts the array from largest to smallest.
+ * (The zeroth Element should be the largest value, and the last element (n-1)
+ * should be the smallest value. )
+
+ *
+ * @param test array of data, source and destination
+ * @param size array length
+ *
+ * @return int - 0 Ok, -1 error
+ */
+
+
+void print_statistics(const unsigned char *test, unsigned int size);
+/**
+ * @brief prints the statistics of an array
+ *
+ * A function that prints the statistics of an array including minimum, maximum, mean, and median.
+ *
+ * @param test array of data
+ * @param size array length
+ *
+ * @return void
+ */
 
 #endif /* __STATS_H__ */
